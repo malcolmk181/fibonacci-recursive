a2b4bb564addfc40a076e3e5c98cc8e913415c99
Malcolm Keyes <3.malcolmk@gmail.com>
2022-07-14 10:37:57 -0400

    Done.

--- javascript/fibonacci_recursive.js
+++ javascript/fibonacci_recursive.js
@@ -1,5 +1,29 @@
-function fibonacci(n) {
+function basic_fibonacci(n) {
   // type your code here
+  switch(n) {
+    case 0:
+      return 0;
+    case 1:
+      return 1;
+    default:
+      return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+}
+
+// dynamic attempt!
+function fibonacci(n) {
+  let results = {};
+
+  function fibonacci_helper(results, n) {
+    if (n < 2) { return n; }
+    else if (results[n]) { return results[n]; }
+    else {
+      results[n] = fibonacci_helper(results, n-1) + fibonacci_helper(results, n-2);
+      return results[n];
+    }
+  }
+
+  return fibonacci_helper(results, n);
 }
 
 if (require.main === module) {
